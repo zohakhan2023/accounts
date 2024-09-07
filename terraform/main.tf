5e46bf8ea86c053d36c21bcd40e0c5dd26103197
zohakhan2023 <136201757+zohakhan2023@users.noreply.github.com>
2024-09-07 17:30:29 -0400

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+
+  backend "s3" {
+    region = "us-east-1"
+    key    = "terraform.tfstate"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "test_instance" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.nano"
+  tags = {
+    Name = "test_instance"
+  }
+}
